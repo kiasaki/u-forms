f6a28a71f0289506eba812b1c5b114e3a2c1318c
Frederic Gingras <frederic@gingras.cc>
2017-03-12 21:31:49 -0400

    make forceSSL carry the path over

--- library/middlewares/forceSSL.js
+++ library/middlewares/forceSSL.js
@@ -2,7 +2,7 @@ async function forceSSL(ctx, next) {
     const config = ctx.container.get("config");
 
     if (!ctx.secure && config.get("node_env") === "production") {
-        ctx.redirect("https://" + config.get("app_base_url"));
+        ctx.redirect("https://" + config.get("app_base_url") + ctx.path);
         return;
     }
 
